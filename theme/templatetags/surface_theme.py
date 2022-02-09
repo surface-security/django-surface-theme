d7376d2577d78b116e29392f85dfad58974bbb63
PPB InfoSec Engineering <surface@paddypowerbetfair.com>
2022-02-09 17:32:00 +0000

    removed unused get_title and other small changes

--- theme/templatetags/surface_theme.py
+++ theme/templatetags/surface_theme.py
@@ -24,14 +24,6 @@ def get_page_range(p, number, on_each_side=3, on_ends=2):
     return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
 
 
-@register.simple_tag
-def get_title(version=True):
-    if settings.TITLE:
-        return f'{settings.TITLE} {settings.VERSION}' if version else settings.TITLE
-    
-    return 'Surface Security'
-
-
 # Helpful with templates to see what's in an object
 @register.filter
 def get_field_content(obj, field):
