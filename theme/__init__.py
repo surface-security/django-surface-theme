5fc011bb2692e0378bfa58915d6d78864c69aa17
Filipe Pina <fopina@gmail.com>
2022-03-23 18:15:58 +0000

    resolve django warnings for AutoField and default_app_config

--- theme/__init__.py
+++ theme/__init__.py
@@ -1,3 +1,10 @@
 __version__ = '0.0.6'
 
-default_app_config = 'theme.apps.SurfaceThemeConfig'
+# set default_app_config when using django earlier than 3.2
+try:
+    import django
+
+    if django.VERSION < (3, 2):
+        default_app_config = 'theme.apps.SurfaceThemeConfig'
+except ImportError:
+    pass
