5b4effff8fbfd15b4fb820cc4d4424a835a83e21
Duarte Duarte <duarte.duarte@blip.pt>
2023-06-13 15:55:40 +0100

    fix displaying site title in login and logout views

--- theme/urls.py
+++ theme/urls.py
@@ -1,4 +1,5 @@
 import django
+from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path, re_path
 from . import views
@@ -14,11 +15,11 @@ def custom_server_error(request):
 
 urlpatterns = [
     # Auth
-    path('login/', views.login_view, name="login"),
-    path('register/', views.register_user, name="register"),
-    path('logout/', LogoutView.as_view(), name="logout"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register_user, name="register"),
+    path("logout/", LogoutView.as_view(extra_context={"site_title": admin.site.site_title}), name="logout"),
     # Model Lookup
-    re_path(r'^model_lookup/$', views.model_lookup_view, name='model_lookup'),
+    re_path(r"^model_lookup/$", views.model_lookup_view, name="model_lookup"),
     # Testing Pages
     path("test-404/", custom_page_not_found),
     path("test-500/", custom_server_error),
