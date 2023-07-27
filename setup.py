dc3a1ea2b0bff166f9ab62496e16b9963ae55f5c
pacuraro <122358966+pacuraro@users.noreply.github.com>
2023-07-27 13:36:00 +0300

    Changed the Django version in if condition (#29)
    Signed-off-by: pacuraro <122358966+pacuraro@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 def get_install_requires():
     install_requires = [
-        'Django>=3,<4',
+        'Django>=3,<5',
         'django-jsoneditor>0.1,<=0.2',
     ]
 
